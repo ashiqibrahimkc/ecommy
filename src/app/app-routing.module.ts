865427c27bed30f84923e7e89eda6aeb17a3bc21
ashiqibrahimkc <ashiqibrahimkarattuchali@gmail.com>
2024-10-14 12:19:19 +0530

    clear lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./website/login/login.component";
+import { SignupComponent } from "./website/signup/signup.component";
+import { VendorloginComponent } from "./vendors/vendorlogin/vendorlogin.component";
+import { VendorsignupComponent } from "./vendors/vendorsignup/vendorsignup.component";
+import { VendorComponent } from "./vendors/vendor/vendor.component";
+import { VendorhomeComponent } from "./vendors/vendorhome/vendorhome.component";
+import { AddproductComponent } from "./vendors/addproduct/addproduct.component";
+import { CategoryComponent } from "./vendors/category/category.component";
+import { CustomersComponent } from "./vendors/customers/customers.component";
+import { MainComponent } from "./website/main/main.component";
+import { HomeComponent } from "./website/home/home.component";
+import { ProductComponent } from "./website/product/product.component";
+import { CartComponent } from "./website/cart/cart.component";
+import { ProductdetailsComponent } from "./website/productdetails/productdetails.component";
+import { ProfileComponent } from "./website/profile/profile.component";
 
 const route:Routes=[
     {path:'',redirectTo:"login",pathMatch:'full'},
-    {path:"login",loadChildren:()=>import('./website/login/login.module').then(m=>m.LoginModule)},
-    {path:"signup",loadChildren:()=>import('./website/signup/signup.module').then(m=>m.SignupModule)},
-    {path:"vendorlogin",loadChildren:()=>import('./vendors/vendorlogin/vendorlogin.module').then(m=>m.VendorLoginModule)},
-    {path:"vendorsignup",loadChildren:()=>import('./vendors/vendorsignup/vendorsignup.module').then(m=>m.VendorSignupModule)},
-    {path:'',loadChildren:()=>import('./vendors/vendor.module').then(m=>m.VendorModules)},
-    {path:'',loadChildren:()=>import('./website/website.module').then(s=>s.WebsiteModule)}
+    {path:"login",component:LoginComponent},
+    {path:"signup",component:SignupComponent},
+    {path:"vendorlogin",component:VendorloginComponent},
+    {path:"vendorsignup",component:VendorsignupComponent},
+    {path:'',component:VendorComponent,children:[
+        {path:'vendor',component:VendorhomeComponent},
+        {path:'addproduct',component:AddproductComponent},
+        {path:'addcategory',component:CategoryComponent},
+        {path:'customers',component:CustomersComponent}
+    ]},
+    {path:'',component:MainComponent,children:[
+        {path:'home',component:HomeComponent},
+        {path:'product',component:ProductComponent},
+        {path:'cart',component:CartComponent},
+        {path:'details',component:ProductdetailsComponent},
+        {path:'profile',component:ProfileComponent}
+    ]}
 ];
 
 @NgModule({
